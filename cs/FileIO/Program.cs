d6d00d868b555cdc2d8526507d5cf1604b0eb9b3
tbgosalvez <tbgosalvez@gmail.com>
2022-08-15 12:22:36 -0400

    file io: enum dirs

---
+++ cs/FileIO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		try
+		{
+			string path = Environment.CurrentDirectory;
+
+			List<string> dirs = new List<string>(Directory.EnumerateDirectories(path));
+
+			foreach(var dd in dirs)
+				Console.WriteLine($"{dd}");
+		}
+		catch(Exception e)
+		{
+			Console.WriteLine(e.Message);
+		}
+	}
+}
